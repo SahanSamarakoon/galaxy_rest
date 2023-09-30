6e053ca2fa84280033a1fffa47192183e9d73569
sahan.samarakoon <sahan.samarakoon.4@gmail.com>
2023-09-30 17:14:51 +0530

    Dynamic Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,13 +9,27 @@ const Cart = props => {
     const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
 
     const hasAmount = totalAmount > 0;
-    const cartItemAddHandler = id => {};
-    const cartItemRemoveHandler = item => {};
+    const cartItemAddHandler = item => {
+        cartContext.addItem({...item, amount:1});
+    };
+    const cartItemRemoveHandler = id => {
+        cartContext.removeItem(id);
+    };
 
-    const cartItems = (<ul className={classes["cart-items"]}>
-        {cartContext.items.map((item) => <CartItem key={item.id} name={item.name} price={item.price}
-                                                   amount={item.amount} onAdd={cartItemAddHandler} onRemove={cartItemRemoveHandler}/>)}
-    </ul>);
+    const cartItems = (
+        <ul className={classes['cart-items']}>
+            {cartContext.items.map((item) => (
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
+            ))}
+        </ul>
+    );
 
 
     return (
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,34 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css"
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = props => {
+    const [btnIsBumped, setBtnIsBumped] = useState(false);
     const cartContext = useContext(CartContext);
-    const { items } = cartContext;
+    const {items} = cartContext;
 
     const numberOfCartItems = items.reduce((currentAmount, item) => {
         return currentAmount + item.amount;
     }, 0);
 
-    return (<button className={classes.button} onClick={props.onClick}>
+    const btnClasses = `${classes.button} ${btnIsBumped ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return
+        }
+        setBtnIsBumped(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsBumped(false);
+        }, 300)
+
+        return ()=> clearTimeout(timer);
+
+    }, [items]);
+
+    return (<button className={btnClasses} onClick={props.onClick}>
         <span>
             <CartIcon/>
         </span>
